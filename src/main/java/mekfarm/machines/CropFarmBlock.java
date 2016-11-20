7b1d5b028b80fe52cb47acf762b614fd276dda96
faceofcat <candreix@gmail.com>
2016-11-20 22:04:18 +0200

    added recipe for crop farm block;

--- src/main/java/mekfarm/machines/CropFarmBlock.java
+++ src/main/java/mekfarm/machines/CropFarmBlock.java
@@ -1,6 +1,12 @@
 package mekfarm.machines;
 
 import mekfarm.common.BlocksRegistry;
+import mekfarm.common.ItemsRegistry;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 
 /**
  * Created by CF on 2016-11-15.
@@ -8,5 +14,18 @@ import mekfarm.common.BlocksRegistry;
 public class CropFarmBlock extends BaseOrientedBlock<CropFarmEntity> {
     public CropFarmBlock() {
         super("crop_farm", CropFarmEntity.class, BlocksRegistry.CROP_FARM_GUI_ID);
+    }
+
+    @Override
+    protected IRecipe getRecipe() {
+        return new ShapedOreRecipe(new ItemStack(this, 1),
+                "xyz", "acb", "www",
+                'x', Items.WHEAT_SEEDS,
+                'y', Items.DIAMOND_HOE,
+                'z', Items.WHEAT_SEEDS,
+                'a', Items.CARROT,
+                'b', Items.POTATO,
+                'c', ItemsRegistry.machineCase,
+                'w', Blocks.PLANKS);
     }
 }
